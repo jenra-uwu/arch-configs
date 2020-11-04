73dfb3823b48693a5b297b1ba51f77f03a608aef
jenra <jenra.uwu@gmail.com>
2020-11-03 19:05:13 -0500

    Modified logout

--- .config/i3/logout.sh
+++ .config/i3/logout.sh
@@ -1,10 +1,12 @@
 #!/bin/fish
 
-if [ (echo -e "logout\ncancel" | rofi -dmenu		\
+if [ (echo -e " Logout\n Cancel" | rofi -dmenu	\
 		-font "System San Francisco 20"				\
 		-lines 2 -hide-scrollbar					\
 		-mesg "Are you sure you wanna log out?"		\
-		-p ""										\
-	) = "logout" ]
+		-p "Logout" -window-title "Logout"			\
+		-selected-row 1	-i							\
+		-only-match -no-custom						\
+	) = " Logout" ]
 	i3-msg exit
 end
