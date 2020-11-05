45fb61e280e87b518a2e7171099862b6f4eb8f7c
jenra <jenra.uwu@gmail.com>
2020-11-05 13:09:49 -0500

    Added !!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,28 @@ alias config='git --git-dir=$HOME/arch-configs.git/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 
 thefuck --alias | source
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
