75c9dca620eb0d5bf359bab8e1e317ec83f5e815
jenra <jenra.uwu@gmail.com>
2020-11-08 21:16:22 -0500

    i dont know

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,9 @@ alias please=sudo
 alias config='git --git-dir=$HOME/arch-configs.git/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
 thefuck --alias | source
 
 function bind_bang
