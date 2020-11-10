d722e87929547e8af4e82a4bda0d7ca59eb11fb2
jenra <jenra.uwu@gmail.com>
2020-11-09 22:15:35 -0500

    Did stuff I guess?? Also screenshots work now

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,8 @@ config config --local status.showUntrackedFiles no
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
+abbr -ag mute-mic pactl set-source-mute @DEFAULT_SOURCE@
+
 thefuck --alias | source
 
 function bind_bang
--- .config/i3/logout.sh
+++ .config/i3/logout.sh
@@ -1,13 +1,14 @@
 #!/bin/fish
 
 if [ (echo -e " Logout\n Cancel" | rofi -dmenu	\
-		-font "System San Francisco 20"				\
+		-font "System San Francisco Display 20"		\
 		-lines 2 -hide-scrollbar					\
 		-mesg "Are you sure you wanna log out?"		\
 		-p "Logout" -window-title "Logout"			\
-		-selected-row 1	-i							\
-		-no-custom									\
+		-selected-row 1	-i						\
+		-no-custom							\
 		-kb-select-1 "Super+Q"						\
 	) = " Logout" ]
 	i3-msg exit
 end
+
