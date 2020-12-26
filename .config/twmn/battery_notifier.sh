fa2554bf08984b98ce7261ccccce2727f9ed34bd
jenra <jenra.uwu@gmail.com>
2020-12-25 21:02:17 -0500

    Fixed battery notifier not responding to power adapter

--- .config/twmn/battery_notifier.sh
+++ .config/twmn/battery_notifier.sh
@@ -1,7 +1,7 @@
 #!/bin/fish
 
 while [ 1 ]
-	if [ (cat /sys/class/power_supply/BAT0/capacity) -lt 10 ]
+	if [ (cat /sys/class/power_supply/ADP1/online) -eq 1 ]; and [ (cat /sys/class/power_supply/BAT0/capacity) -lt 10 ]
 		command notify-send 'Low battery'
 	end
 end
