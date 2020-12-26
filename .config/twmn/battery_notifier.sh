d75ab777f5802cd2180f9be939fa6223e88dbeb8
jenra <jenra.uwu@gmail.com>
2020-12-25 20:47:51 -0500

    Changed battery notifier to be more annoying

--- .config/twmn/battery_notifier.sh
+++ .config/twmn/battery_notifier.sh
@@ -1,12 +1,8 @@
 #!/bin/fish
 
-set sent_notification 0
 while [ 1 ]
-	if [ (cat /sys/class/power_supply/ADP1/online) -eq 1 ]
-		set sent_notification 0
-	else if [ (cat /sys/class/power_supply/BAT0/capacity) -lt 10 ] && [ $sent_notification -eq 0 ]
+	if [ (cat /sys/class/power_supply/BAT0/capacity) -lt 10 ]
 		command notify-send 'Low battery'
-		set sent_notification 1
 	end
 end
 
