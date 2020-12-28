da370006852317769094de966c664ced04292e08
jenra <jenra.uwu@gmail.com>
2020-12-28 10:28:24 -0500

    Switched to dunst

--- .config/twmn/battery_notifier.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/fish
-
-while [ 1 ]
-	if [ (cat /sys/class/power_supply/ADP1/online) -eq 0 ]; and [ (cat /sys/class/power_supply/BAT0/capacity) -lt 10 ]
-		command notify-send 'Low battery'
-	end
-	command sleep 60
-end
-
