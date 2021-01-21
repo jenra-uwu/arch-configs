1aaa5c9c6657b4fee700aae21c26a78bee732271
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-01-21 15:31:46 -0500

    Did stuff

--- .config/dunst/battery_notifier.sh
+++ .config/dunst/battery_notifier.sh
@@ -1,7 +1,7 @@
 #!/bin/fish
 
 while [ 1 ]
-	if [ (cat /sys/class/power_supply/BAT0/capacity) -lt 10 ] && [ (cat /sys/class/power_supply/ADP1/online) -eq 1 ]
+	if [ (cat /sys/class/power_supply/BAT0/capacity) -lt 10 ] && [ (cat /sys/class/power_supply/ADP1/online) -eq 0 ]
 		command notify-send 'Low battery' 'Please plug in your power adapter' --urgency=critical --app-name=Battery\ Status
 	end
 	command sleep 60
