857bdde9d8dfb0c9ad199161f16d40c270b5c4b0
jenra <a>
2021-04-07 20:30:55 -0400

    Did stuff

--- .config/polybar/microphone-mute.sh
+++ .config/polybar/microphone-mute.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/fish
 
-set muted (pactl list sources | sed -n '/\s*State: RUNNING/, /^$/p' | grep Mute)
+set muted (pactl list sources | sed -n '/Name: alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_6__source/, /^$/p' | grep 'Mute: no')
 
-if [ "$muted" = '	Mute: no' ]
+if [ $muted ]
 	echo ''
 else
 	echo ''
